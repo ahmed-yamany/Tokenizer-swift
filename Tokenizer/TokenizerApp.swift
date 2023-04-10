b609f52cb93d0c0d8fb6d5ba46a02a755c288472
Ahmed Yamany <yamany2862016@gmail.com>
2023-04-10 12:07:50 +0200

    Initial Commit

---
+++ Tokenizer/TokenizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenizerApp.swift
+//  Tokenizer
+//
+//  Created by Ahmed Yamany on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TokenizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
